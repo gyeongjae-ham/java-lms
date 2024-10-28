7102ae95fba603a78f12972c0a6018f777a5ec3f
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-28 12:45:14 +0900

    feat: 강의 상태 추가 및 모집중일때만 등록 검증
    강의 상태 추가 및 모집중일때만 등록 검증

--- src/main/java/nextstep/courses/domain/FreeSession.java
+++ src/main/java/nextstep/courses/domain/FreeSession.java
@@ -6,11 +6,13 @@ public class FreeSession extends Session {
 
     private final Long id;
     private final Students students;
+    private final SessionStatus status;
 
     public FreeSession(
         Long id,
         SessionDate sessionDate,
         SessionImage sessionImage,
+        SessionStatus sessionStatus,
         Type sessionType)
     {
         super(sessionDate,
@@ -18,9 +20,13 @@ public class FreeSession extends Session {
             sessionType);
         this.id = id;
         this.students = new Students();
+        this.status = sessionStatus;
     }
 
     public void addStudent(NsUser student) {
+        if (!(status == SessionStatus.RESITER)) {
+            throw new IllegalStateException("This session is not registering now");
+        }
         students.add(student);
     }
 }
--- src/test/java/nextstep/courses/domain/FreeSessionTest.java
+++ src/test/java/nextstep/courses/domain/FreeSessionTest.java
@@ -4,11 +4,22 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import nextstep.payments.domain.Payment;
+import nextstep.users.domain.NsUserTest;
+
 public class FreeSessionTest {
 
     @Test
     void 생성() {
-        Session freeSession = SessionTest.createFreeSession();
-        assertThat(freeSession).isEqualTo(SessionTest.createFreeSession());
+        FreeSession freeSession = SessionTest.createFreeSession(SessionStatus.PREPARE);
+        assertThat(freeSession).isEqualTo(SessionTest.createFreeSession(SessionStatus.PREPARE));
+    }
+
+    @Test
+    void 모집중이_아니면_등록하지_못한다() {
+        FreeSession freeSession = SessionTest.createFreeSession(SessionStatus.PREPARE);
+
+        assertThatThrownBy(() -> freeSession.addStudent(NsUserTest.GYEONGJAE))
+            .isInstanceOf(IllegalStateException.class);
     }
 }
