9ad2045eece9b735ce583d410e88861d5cda482d
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-10-31 12:05:21 +0900

    refactor: 이미지 목록 불러와서 Session 완성하기
    이미지 목록 불러와서 Session 완성하기

--- src/main/java/nextstep/courses/service/SessionService.java
+++ src/main/java/nextstep/courses/service/SessionService.java
@@ -9,7 +9,12 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import nextstep.courses.domain.Image;
+import nextstep.courses.domain.ImageRepository;
+import nextstep.courses.domain.Images;
 import nextstep.courses.domain.Session;
+import nextstep.courses.domain.SessionImage;
+import nextstep.courses.domain.SessionImageRepository;
 import nextstep.courses.domain.SessionRepository;
 import nextstep.courses.domain.SessionStudent;
 import nextstep.courses.domain.SessionStudentRepository;
@@ -27,22 +32,37 @@ public class SessionService {
     @Resource(name = "sessionStudentRepository")
     private SessionStudentRepository sessionStudentRepository;
 
+    @Resource(name = "sessionImageRepository")
+    private SessionImageRepository sessionImageRepository;
+
     @Resource(name = "userRepository")
     private UserRepository userRepository;
+
+    @Resource(name = "imageRepository")
+    private ImageRepository imageRepository;
 
     @Transactional
     public Session findSessionWithId(long sessionId) {
         Session session = sessionRepository.findById(sessionId).orElseThrow(NotFoundException::new);
+
         List<Long> studentIds = sessionStudentRepository.findAllBySessionId(sessionId).stream()
             .map(SessionStudent::getStudentId)
             .collect(Collectors.toList());
-
         List<NsUser> studentList = new ArrayList<>();
         for (Long studentId : studentIds) {
-            NsUser nsUser = userRepository.findById(studentId).orElseThrow(NotFoundException::new);
-            studentList.add(nsUser);
+            studentList.add(userRepository.findById(studentId).orElseThrow(NotFoundException::new));
         }
+
+        List<Long> imageIds = sessionImageRepository.findAllBySessionId(sessionId).stream()
+            .map(SessionImage::getImageId)
+            .collect(Collectors.toList());
+        List<Image> imageList = new ArrayList<>();
+        for (Long imageId : imageIds) {
+            imageList.add(imageRepository.findById(imageId).orElseThrow(NotFoundException::new));
+        }
+
         session.addStudents(new Students(sessionId, studentList));
+        session.addImages(new Images(sessionId, imageList));
 
         return session;
     }
