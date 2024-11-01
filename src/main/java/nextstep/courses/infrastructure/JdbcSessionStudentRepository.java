140f644caa22920d633cf28a7d1b36d2ae666a3c
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-11-01 09:45:21 +0900

    feat: 콜렉션으로 받아서 저장하도록 메소드 추가
    콜렉션으로 받아서 저장하도록 메소드 추가

--- src/main/java/nextstep/courses/infrastructure/JdbcSessionStudentRepository.java
+++ src/main/java/nextstep/courses/infrastructure/JdbcSessionStudentRepository.java
@@ -25,7 +25,7 @@ public class JdbcSessionStudentRepository implements SessionStudentRepository {
 
     @Override
     public int[] saveAll(Students students) {
-        String sql = "insert into session_student2 (session_id, student_id, session_student_status) values (?, ?, ?)";
+        String sql = "insert into session_student (session_id, student_id, session_student_status) values (?, ?, ?)";
 
         List<SessionStudent> sessionStudents = students.getStudents().stream()
             .map(it -> new SessionStudent(students.getSessionId(), it.getId()))
@@ -48,8 +48,28 @@ public class JdbcSessionStudentRepository implements SessionStudentRepository {
     }
 
     @Override
+    public int[] saveAll(List<SessionStudent> sessionStudents) {
+        String sql = "insert into session_student (session_id, student_id, session_student_status) values (?, ?, ?)";
+
+        return jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                SessionStudent sessionStudent = sessionStudents.get(i);
+                ps.setLong(1, sessionStudent.getSessionId());
+                ps.setLong(2, sessionStudent.getStudentId());
+                ps.setString(3, sessionStudent.getSessionStudentStatus().toString());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return sessionStudents.size();
+            }
+        });
+    }
+
+    @Override
     public List<SessionStudent> findAllBySessionId(long sessionId) {
-        String sql = "select id, session_id, student_id, session_student_status from session_student2 where session_id = ?";
+        String sql = "select id, session_id, student_id, session_student_status from session_student where session_id = ?";
 
         RowMapper<SessionStudent> rowMapper = (rs, rowNum) -> new SessionStudent(
             rs.getLong("id"),
