b2d5dd85e4b9a56e946881236d7e725849eb8de2
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2024-11-01 09:45:08 +0900

    refactor: 테이블 명 수정
    테이블 명 수정

--- src/main/java/nextstep/courses/infrastructure/JdbcSessionRepository.java
+++ src/main/java/nextstep/courses/infrastructure/JdbcSessionRepository.java
@@ -26,8 +26,9 @@ public class JdbcSessionRepository implements SessionRepository {
 
     @Override
     public int save(Session session) {
-        String sql = "insert into new_session2 (course_id, teacher_id, price, session_status, register_status, max_student_size, start_at, end_at, type) "
-            + "values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String sql =
+            "insert into new_session (course_id, teacher_id, price, session_status, register_status, max_student_size, start_at, end_at, type) "
+                + "values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
         return jdbcTemplate.update(sql,
             session.getCourseId(),
             session.getTeacherId(),
@@ -42,29 +43,29 @@ public class JdbcSessionRepository implements SessionRepository {
 
     @Override
     public Optional<Session> findById(Long id) {
-        String sql = "select id, course_id, teacher_id, price, session_status, register_status, max_student_size, start_at, end_at, type from new_session2 where id = ?";
+        String sql = "select id, course_id, teacher_id, price, session_status, register_status, max_student_size, start_at, end_at, type from new_session where id = ?";
 
         RowMapper<Session> rowMapper = (rs, rowNum) -> new Session(
-                rs.getLong(1),
-                rs.getLong(2),
-                rs.getLong(3),
-                rs.getLong(4),
-                SessionStatus.from(rs.getString(5)),
-                RegisterStatus.from(rs.getString(6)),
-                rs.getInt(7),
-                new SessionDate(
-                    toLocalDateTime(rs.getTimestamp(8)),
-                    toLocalDateTime(rs.getTimestamp(9))
-                ),
-                Type.from(rs.getString(10))
-            );
+            rs.getLong(1),
+            rs.getLong(2),
+            rs.getLong(3),
+            rs.getLong(4),
+            SessionStatus.from(rs.getString(5)),
+            RegisterStatus.from(rs.getString(6)),
+            rs.getInt(7),
+            new SessionDate(
+                toLocalDateTime(rs.getTimestamp(8)),
+                toLocalDateTime(rs.getTimestamp(9))
+            ),
+            Type.from(rs.getString(10))
+        );
 
         return Optional.ofNullable(jdbcTemplate.queryForObject(sql, rowMapper, id));
     }
 
     @Override
     public List<Session> findByTeacherId(Long teacherId) {
-        String sql = "select id, course_id, teacher_id, price, session_status, register_status, max_student_size, start_at, end_at, type from new_session2 where teacher_id = ?";
+        String sql = "select id, course_id, teacher_id, price, session_status, register_status, max_student_size, start_at, end_at, type from new_session where teacher_id = ?";
 
         RowMapper<Session> rowMapper = (rs, rowNum) -> new Session(
             rs.getLong(1),
